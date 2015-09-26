ab1df8f6602f5c55731606f88665c37c2f39097e
chivalrous <chivalrousljb@gmail.com>
2015-09-26 10:20:03 +0800

    LinkedList
    LinkedList

---
+++ ListNodePartition.java
@@ -0,0 +1,63 @@
+package nowCoder;
+import java.util.*;
+/*
+ * 题目描述
+编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
+给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。注意：分割以后保持原来的数据顺序不变。
+ */
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+
+public class ListNodePartition {
+	  public ListNode partition(ListNode pHead, int x) {
+	        // write code here
+		  ListNode beforeStart = null;
+		  ListNode beforeEnd = null;
+		  ListNode afterStart = null;
+		  ListNode afterEnd = null;
+		  
+		  while(pHead != null)
+		  {
+			  ListNode next = pHead.next;
+			  pHead.next = null;
+			  if(pHead.val < x)
+			  {
+				  if(beforeStart == null)
+				  {
+					  beforeStart = pHead;
+					  beforeEnd = beforeStart;
+				  }
+				  else 
+				  {
+					  beforeEnd.next = pHead;
+					  beforeEnd = pHead;
+				  }
+			  }
+			  else {
+				if(afterStart == null)
+				{
+					afterStart = pHead;
+					afterEnd = afterStart;
+				}
+				else {
+					afterEnd.next = pHead;
+					afterEnd = pHead;
+				}
+			}
+			  pHead = next;
+		  }
+		  
+		  if(beforeStart == null)
+			  return afterStart;
+		  
+		  beforeEnd.next = afterStart;
+		  return beforeStart;
+	 }
+}
