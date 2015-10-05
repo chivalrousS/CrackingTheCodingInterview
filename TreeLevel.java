3e57ead38a8b78ecc83e4e8ea744a2ac36239f8f
chivalrous <719583248@qq.com>
2015-10-05 19:59:27 +0800

    Tree
    Tree

--- TreeLevel.java
+++ TreeLevel.java
@@ -1,5 +1,28 @@
 package chapter9;
+/*
+ * 题目描述
+对于一棵二叉树，请设计一个算法，创建含有某一深度上所有结点的链表。
+给定二叉树的根结点指针TreeNode* root，以及链表上结点的深度，请返回一个链表ListNode，代表该深度上所有结点的值，
+请按树上从左往右的顺序链接，保证深度不超过树的高度，树上结点的值为非负整数且不超过100000。
+ */
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
 
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}*/
 public class TreeLevel {
 	 public ListNode getTreeLevel(TreeNode root, int dep) {
 	        // write code here
